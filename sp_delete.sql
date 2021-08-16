9b271b7722cc68d79c4d4171a7a8e796c27d54d6
Marisha <75089445+MarizzaM@users.noreply.github.com>
2021-08-16 15:17:45 +0300

    Update sp_delete.sql

--- sp_delete.sql
+++ sp_delete.sql
@@ -20,3 +20,28 @@ language plpgsql AS
     $$;
 
 select * from sp_delete(5);
+
+
+-- with CASCADE
+
+drop function sp_delete;
+
+create or replace function sp_delete(_id bigint) returns int
+
+language plpgsql AS
+    $$
+        DECLARE
+            rows_count int := 0;
+        BEGIN
+
+            WITH row AS (
+            DELETE FROM customers
+            WHERE customers.id = _id
+            RETURNING 1)
+            SELECT count(*) INTO rows_count FROM row;
+            return rows_count;
+
+        END;
+    $$;
+
+select * from sp_delete(6);
